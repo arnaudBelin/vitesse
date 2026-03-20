40c259a6bd0c643291f97053a6f4db2cceeb823a
Arnold <arnaud.belin@fizzup.com>
2026-03-20 10:18:27 +0100

    Add DAO & setup DB

---
+++ app/src/main/java/com/example/vitesse/data/db/VitesseDB.kt
@@ -0,0 +1,34 @@
+package com.example.vitesse.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.vitesse.data.dao.CandidateDao
+import com.example.vitesse.data.entity.Candidate
+import com.example.vitesse.data.utils.Converters
+
+const val DATABASE_VERSION = 1
+
+@Database(entities = [Candidate::class], version = DATABASE_VERSION)
+@TypeConverters(Converters::class)
+abstract class VitesseDB : RoomDatabase()
+{
+    companion object {
+        @Volatile
+        private var INSTANCE: VitesseDB? = null
+
+        fun getDatabase(context: Context): VitesseDB {
+            return INSTANCE ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    VitesseDB::class.java,
+                    "vitesse_db"
+                ).build().also { INSTANCE = it }
+            }
+        }
+    }
+
+    abstract fun candidateDao(): CandidateDao
+}
---
+++ app/src/main/java/com/example/vitesse/data/entity/Candidate.kt
@@ -0,0 +1,32 @@
+package com.example.vitesse.data.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.example.vitesse.data.utils.Converters
+import java.time.LocalDate
+import java.time.Instant
+
+@Entity
+@TypeConverters(Converters::class)
+data class Candidate(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "first_name")
+    val firstName: String,
+    @ColumnInfo(name = "last_name")
+    val lastName: String,
+    val phone: String,
+    val email: String,
+    @ColumnInfo(name = "birth_date")
+    val birthDate: LocalDate,
+    val salary: Double? = null, // in Euros
+    val note: String? = null,
+    @ColumnInfo(name = "is_favorite")
+    val isFavorite: Boolean = false,
+    @ColumnInfo(name = "created_at")
+    val createdAt: Instant,
+    @ColumnInfo(name = "deleted_at")
+    val deletedAt: Instant? = null,
+)
