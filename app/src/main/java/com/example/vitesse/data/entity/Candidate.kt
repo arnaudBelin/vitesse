57caad6e86d7e88743b96e6bc22e6b03048ae4da
Arnold <arnaud.belin@fizzup.com>
2026-03-20 12:18:53 +0100

    Add picture

--- app/src/main/java/com/example/vitesse/data/entity/Candidate.kt
+++ app/src/main/java/com/example/vitesse/data/entity/Candidate.kt
@@ -19,6 +19,7 @@ data class Candidate(
     val lastName: String,
     val phone: String,
     val email: String,
+    val picture: String? = null,
     @ColumnInfo(name = "birth_date")
     val birthDate: LocalDate,
     val salary: Double? = null, // in Euros
