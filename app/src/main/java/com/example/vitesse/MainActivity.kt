7f81afcad7146f5f4ce85cf823c028b7b2516395
Arnold <arnaud.belin@fizzup.com>
2026-03-20 12:19:07 +0100

    Add viewModel in Activity

--- app/src/main/java/com/example/vitesse/MainActivity.kt
+++ app/src/main/java/com/example/vitesse/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,12 +15,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.vitesse.ui.home.MainActivityViewModel
 import com.example.vitesse.ui.theme.VitesseTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        viewModel.fetchCandidates()
+
         setContent {
             VitesseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()
