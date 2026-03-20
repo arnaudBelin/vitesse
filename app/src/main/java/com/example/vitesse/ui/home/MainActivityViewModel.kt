b75f793dca9660450f32e2c3f41c4c2a5cc48580
Arnold <arnaud.belin@fizzup.com>
2026-03-20 12:07:20 +0100

    Add ViewModel

---
+++ app/src/main/java/com/example/vitesse/ui/home/MainActivityViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.vitesse.ui.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.vitesse.data.entity.Candidate
+import com.example.vitesse.data.repository.CandidateRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainActivityViewModel @Inject constructor(
+    private val candidateRepository: CandidateRepository
+) : ViewModel() {
+    private val _candidateState = MutableStateFlow<List<Candidate>>(emptyList())
+    val candidateState: StateFlow<List<Candidate>> = _candidateState
+
+    fun fetchCandidates() {
+        viewModelScope.launch(Dispatchers.IO) {
+            candidateRepository.getAllCandidates().collect { candidates ->
+                _candidateState.value = candidates
+            }
+        }
+    }
+
+
+
+}
