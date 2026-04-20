47fe8f8cf45a1e82e2e0038fcc4e9a72228ace60
Arnold <arnaud.belin@fizzup.com>
2026-04-20 20:03:39 +0200

    Add Test CCurrency

---
+++ app/src/test/java/com/example/vitesse/CurrencyRepositoryTest.kt
@@ -0,0 +1,34 @@
+package com.example.vitesse
+
+import app.cash.turbine.test
+import com.example.vitesse.data.model.CurrencyRates
+import com.example.vitesse.data.network.CurrencyClient
+import com.example.vitesse.data.repository.CurrencyRepository
+import com.example.vitesse.data.response.CurrencyResponse
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class CurrencyRepositoryTest {
+
+    @Test
+    fun testFetchCurrencyDataMappedCurrencyModel() = runTest {
+        // Given
+        val fakeClient = object : CurrencyClient {
+            override suspend fun getRates(): CurrencyResponse {
+                return CurrencyResponse(
+                    date = "2025-04-20",
+                    eur = CurrencyRates(gbp = 0.86)
+                )
+            }
+        }
+        val repository = CurrencyRepository(fakeClient)
+
+        // When / Then
+        repository.fetchCurrencyData().test {
+            val result = awaitItem()
+            assertEquals(0.86, result.rates.gbp)
+            awaitComplete()
+        }
+    }
+}
---
+++ app/src/test/java/com/example/vitesse/CurrencyResponseTest.kt
@@ -0,0 +1,24 @@
+package com.example.vitesse
+
+import com.example.vitesse.data.model.CurrencyRates
+import com.example.vitesse.data.response.CurrencyResponse
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class CurrencyResponseTest {
+
+    @Test
+    fun testToCurrencyModel() {
+        // Given
+        val response = CurrencyResponse(
+            date = "2025-04-20",
+            eur = CurrencyRates(gbp = 0.86)
+        )
+
+        // When
+        val result = response.toCurrencyModel()
+
+        // Then
+        assertEquals(0.86, result.rates.gbp)
+    }
+}
